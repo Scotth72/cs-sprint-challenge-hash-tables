c7b116d6c3d709d67f58376032177f71a84074c4
Heath Scott <you@example.com>
2020-07-10 10:22:26 -0500

    Initial commit

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -2,6 +2,9 @@ def get_indices_of_item_weights(weights, length, limit):
     """
     YOUR CODE HERE
     """
-    # Your code here
+    # need to store weights in cache
+    # loop through the list of weights
+
+    weight_cache = {weights[i]: i for i in range(len(weights))}
 
     return None
