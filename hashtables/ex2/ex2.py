7f865226fd042fbab7a89179f9ed075ea7069098
Heath Scott <you@example.com>
2020-07-10 12:17:27 -0500

    three done for MVP, working on next one

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -11,4 +11,22 @@ def reconstruct_trip(tickets, length):
     """
     # Your code here
 
+    # intialize the array to have starting point
+    # loop through
+    # have start point
+
+    route_start = {}
+    route = []
+
+    for ticket in tickets:
+        route_start[ticket.source] = ticket.destination
+
+    new_destination = route_start["NONE"]
+
+    while new_destination != "NONE":
+        route.append(new_destination)
+        new_destination = route_start[new_destination]
+
+    route.append("NONE")
+
     return route
